2111b757dc4cfc22d946ada7d93bb049b816c39c
liuhuijun <liuhuijun_2017@163.com>
2019-05-14 23:36:00 +0800

    链表-插入相关代码。

--- src/main/java/com/joy/linkedlist/SinglyLinkedList.java
+++ src/main/java/com/joy/linkedlist/SinglyLinkedList.java
@@ -33,6 +33,8 @@ public class SinglyLinkedList {
         insertToHead(newNode);
     }
 
+    //无头节点
+    //表头部插入，可以将输入的顺序进行逆序
     public void insertToHead(Node newNode){
         if(head == null) head = newNode;
         else{
@@ -40,6 +42,50 @@ public class SinglyLinkedList {
             head = newNode;
         }
     }
+
+    public void insertToTail(Node newNode){
+        if(head == null) head = newNode;
+        else{
+            Node q = head;
+            while (q.next != null){
+                q = q.next;
+            }
+            newNode.next = q.next;
+            q.next = newNode;
+        }
+    }
+
+    public void insertAfter(Node p,int value){
+        Node newNode = new Node(value,null);
+        insertAfter(p,newNode);
+    }
+
+    public void insertAfter(Node p,Node newNode){
+        if(p == null) return;
+        newNode.next = p;
+        p.next = newNode;
+    }
+
+    public void insertBefore(Node p,int value){
+        Node newNode = new Node(value,null);
+        insertBefore(p,newNode);
+    }
+
+    public void insertBefore(Node p,Node newNode){
+        if( p == null) return;
+        if( p == head) insertToHead(newNode);
+        else{
+            Node q = head;
+            if(q == null) return;
+            while (q.next != p){
+                q = q.next;
+            }
+            q.next = newNode;
+            newNode.next = p;
+        }
+    }
+
+    
 
     public static class Node{
         private int data;
