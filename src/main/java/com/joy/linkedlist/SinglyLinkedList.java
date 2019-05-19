fed41f3c0857830c09c4c6223cc1fe54aa1bffe7
liuhuijun <liuhuijun_2017@163.com>
2019-05-19 23:12:02 +0800

    链表-删除操作相关代码。

--- src/main/java/com/joy/linkedlist/SinglyLinkedList.java
+++ src/main/java/com/joy/linkedlist/SinglyLinkedList.java
@@ -12,42 +12,42 @@ public class SinglyLinkedList {
 
     public Node findByValue(int value) {
         Node p = head;
-        while(p != null && p.data != value){
+        while (p != null && p.data != value) {
             p = p.next;
         }
         return p;
     }
 
-    public Node findByIndex(int index){
+    public Node findByIndex(int index) {
         Node p = head;
         int pos = 0;
-        while(p != null && pos != index){
+        while (p != null && pos != index) {
             p = p.next;
             pos++;
         }
         return p;
     }
 
-    public void insertToHead(int value){
+    public void insertToHead(int value) {
         Node newNode = new Node(value, null);
         insertToHead(newNode);
     }
 
     //无头节点
     //表头部插入，可以将输入的顺序进行逆序
-    public void insertToHead(Node newNode){
-        if(head == null) head = newNode;
-        else{
+    public void insertToHead(Node newNode) {
+        if (head == null) head = newNode;
+        else {
             newNode.next = head;
             head = newNode;
         }
     }
 
-    public void insertToTail(Node newNode){
-        if(head == null) head = newNode;
-        else{
+    public void insertToTail(Node newNode) {
+        if (head == null) head = newNode;
+        else {
             Node q = head;
-            while (q.next != null){
+            while (q.next != null) {
                 q = q.next;
             }
             newNode.next = q.next;
@@ -55,29 +55,29 @@ public class SinglyLinkedList {
         }
     }
 
-    public void insertAfter(Node p,int value){
-        Node newNode = new Node(value,null);
-        insertAfter(p,newNode);
+    public void insertAfter(Node p, int value) {
+        Node newNode = new Node(value, null);
+        insertAfter(p, newNode);
     }
 
-    public void insertAfter(Node p,Node newNode){
-        if(p == null) return;
+    public void insertAfter(Node p, Node newNode) {
+        if (p == null) return;
         newNode.next = p;
         p.next = newNode;
     }
 
-    public void insertBefore(Node p,int value){
-        Node newNode = new Node(value,null);
-        insertBefore(p,newNode);
+    public void insertBefore(Node p, int value) {
+        Node newNode = new Node(value, null);
+        insertBefore(p, newNode);
     }
 
-    public void insertBefore(Node p,Node newNode){
-        if( p == null) return;
-        if( p == head) insertToHead(newNode);
-        else{
+    public void insertBefore(Node p, Node newNode) {
+        if (p == null) return;
+        if (p == head) insertToHead(newNode);
+        else {
             Node q = head;
-            if(q == null) return;
-            while (q.next != p){
+            if (q == null) return;
+            while (q.next != p) {
                 q = q.next;
             }
             q.next = newNode;
@@ -85,9 +85,21 @@ public class SinglyLinkedList {
         }
     }
 
-    
+    public void deleteByNode(Node p) {
+        if (p == null || head == null) return;
+        if (p == head) {
+            head = head.next;
+            return;
+        }
+        Node q = head;
+        while(q != null || q.next != p){
+            q = q.next;
+        }
+        q.next = q.next.next;
+    }
 
-    public static class Node{
+
+    public static class Node {
         private int data;
         private Node next;
 
