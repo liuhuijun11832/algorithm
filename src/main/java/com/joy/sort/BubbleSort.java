08aa91239981322399338440e36e5b7c395479f0
刘会俊 <liuhuijun_2017@163.com>
2020-11-09 08:50:32 +0800

    feat:冒泡排序

--- src/main/java/com/joy/sort/BubbleSort.java
+++ src/main/java/com/joy/sort/BubbleSort.java
@@ -8,8 +8,33 @@ package com.joy.sort;
  */
 public class BubbleSort {
 
-    public int[] bubbleSort(int[] originalArray){
-        return null;
+    public static int[] bubbleSort(int[] originalArray){
+        if (originalArray == null) {
+            return null;
+        }
+        for (int i = 0; i < originalArray.length; i++) {
+            int j = i + 1;
+            boolean isChanged = false;
+            for (; j < originalArray.length; j++) {
+                if(originalArray[j] < originalArray[i]){
+                    int temp = originalArray[j];
+                    originalArray[j] = originalArray[i];
+                    originalArray[i] = temp;
+                    isChanged = true;
+                }
+            }
+            if(!isChanged){
+                break;
+            }
+        }
+        return originalArray;
     }
 
+    public static void main(String[] args) {
+        int[] originalArray = new int[]{2, 3, 4, 6, 1, 5, 10, 9, 8, 7};
+        int[] afterSort = bubbleSort(originalArray);
+        for (int i : afterSort) {
+            System.out.println(i);
+        }
+    }
 }
