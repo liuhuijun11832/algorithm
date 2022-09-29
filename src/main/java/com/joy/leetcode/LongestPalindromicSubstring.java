9029e694a4402fb003c6054a65a3e7a61b1779d7
刘会俊 <liuhuijun_2017@163.com>
2022-09-29 20:15:53 +0800

    feat:无重复字符串最大长度

---
+++ src/main/java/com/joy/leetcode/LongestPalindromicSubstring.java
@@ -0,0 +1,19 @@
+package com.joy.leetcode;
+
+/**
+ * 最长回文
+ */
+public class LongestPalindromicSubstring {
+
+    public static void main(String[] args) {
+        LongestPalindromicSubstring longestPalindromicSubstring = new LongestPalindromicSubstring();
+        System.out.println(longestPalindromicSubstring.longestPalindrome("babbab"));
+    }
+
+    public String longestPalindrome(String s) {
+        int length = s.length();
+        char[] chars = new char[length];
+
+    }
+
+}
