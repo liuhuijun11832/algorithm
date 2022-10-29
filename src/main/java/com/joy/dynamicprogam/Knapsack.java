cb5730b38f0295453b8b0055cfcd19cb2bafd700
刘会俊 <liuhuijun_2017@163.com>
2022-10-29 17:58:19 +0800

    feat:动态规划

--- src/main/java/com/joy/dynamicprogam/Knapsack.java
+++ src/main/java/com/joy/dynamicprogam/Knapsack.java
@@ -9,36 +9,40 @@ package com.joy.dynamicprogam;
 public class Knapsack {
 
     public static void main(String[] args) {
-
+        System.out.println( pack(new int[] {2, 2, 4, 6, 3}, 5, 9));
     }
 
     /**
      * 打包
+     *
      * @param weights 物品数组
-     * @param n 物品数量
-     * @param w 背包最大重量
+     * @param n       物品数量
+     * @param w       背包最大重量
      * @return
      */
-    public static int pack(int[] weights, int n, int w){
+    public static int pack(int[] weights, int n, int w) {
         boolean[][] states = new boolean[n][w + 1];
         states[0][0] = true;
-        if(weights[0] <= w){
+        if (weights[0] <= w) {
             states[0][weights[0]] = true;
         }
         for (int i = 1; i < n; i++) {
             for (int j = 0; j <= w; j++) {
+                // 第i个物品不放入背包，则第i个物品的重量和第i减1个物品的重量一致
+                // 这样避免了多一次计算
                 if (states[i - 1][j]) {
                     states[i][j] = true;
                 }
             }
+            // 如果第i个物品放入背包，根据上次是否放入，计算本次的重量
             for (int j = 0; j <= w - weights[i]; j++) {
-                if(states[i-1][j]){
+                if (states[i - 1][j]) {
                     states[i][j + weights[i]] = true;
                 }
             }
         }
         for (int i = w; i >= 0; i--) {
-            if (states[n-1][i]) {
+            if (states[n - 1][i]) {
                 return i;
             }
         }
--- src/main/java/com/joy/dynamicprogam/KnapsackSimplify.java
+++ src/main/java/com/joy/dynamicprogam/KnapsackSimplify.java
@@ -3,7 +3,7 @@ package com.joy.dynamicprogam;
 /**
  * 动态规划-背包问题-简化版代码
  * 满足背包最大重量的前提下，背包中物品总重量的最大值
- *
+ * 实际上就是每次
  * @author Joy
  * @date 2022-10-29
  */
@@ -13,17 +13,18 @@ public class KnapsackSimplify {
         System.out.println( pack(new int[] {2, 2, 4, 6, 3}, 5, 9));
     }
 
-    public static int pack(int[] items, int n, int w){
+    public static int pack(int[] weights, int n, int w){
         boolean[] states = new boolean[w + 1];
         states[0] = true;
-        if (items[0] <= w) {
-            states[items[0]] = true;
+        if (weights[0] <= w) {
+            states[weights[0]] = true;
         }
 
+        // 简化的地方在于，数组只记录上一次的重量数据，每次放入一个新物品，只需要判断上次是否放入，然后写入本次的值
         for (int i = 0; i < n; i++) {
-            for (int j = w - items[i]; j >= 0; j--) {
+            for (int j = w - weights[i]; j >= 0; j--) {
                 if (states[j]) {
-                    states[j + items[i]] = true;
+                    states[j + weights[i]] = true;
                 }
             }
         }
