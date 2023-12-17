ac9ad27e83672d07b809add210bea049ad22be8a
刘会俊 <liuhuijun_2017@163.com>
2023-12-17 22:48:09 +0800

    feat:最长回文串

--- src/main/java/com/joy/leetcode/LongestPalindromicSubstring.java
+++ src/main/java/com/joy/leetcode/LongestPalindromicSubstring.java
@@ -12,9 +12,32 @@ public class LongestPalindromicSubstring {
     }
 
     public String longestPalindrome(String s) {
-        int length = s.length();
-        char[] chars = new char[length];
-        return null;
+        char[] chars = s.toCharArray();
+        int start = 0, len = 0;
+        for (int i = 0; i < chars.length; i++) {
+            int n1 = extrated(chars, i, i);
+            int n2 = extrated(chars, i, i + 1);
+            int n = Math.max(n1, n2);
+            if(n > len){
+                len = n;
+                start = i - (len - 1) / 2;
+            }
+        }
+        return s.substring(start, start + len);
+    }
+
+    public int extrated(char[] chars, int left, int right){
+        int length = chars.length;
+        while (left >= 0 && right < length){
+            if(chars[left] != chars[right]){
+                break;
+            }
+            if(chars[left] == chars[right]){
+                left--;
+                right++;
+            }
+        }
+        return right - left - 1;
     }
 
 }
